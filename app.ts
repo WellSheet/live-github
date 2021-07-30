8911d1403be1893790c4bdbf9ffc18f7e62dfafa
Corey Alexander <coreyja@gmail.com>
2021-07-29 23:30:34 -0400

    Oops need to compile locally lol

--- app.ts
+++ app.ts
@@ -176,7 +176,7 @@ webhooks.on("pull_request_review_comment.created", async ({payload}) => {
 
     const githubCommentText = `We made a thread for you! Check it out here: ${threadUrlResponse.permalink}`
 
-    await postReviewComentReply(githubApp, pull_request.number, originalGithubComment.id, githubCommentText)
+    await postReviewComentReply(githubApp, pull_request.number, comment.in_reply_to_id || comment.id, githubCommentText)
   }
 });
 
