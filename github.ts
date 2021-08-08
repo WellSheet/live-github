c9f30f7c40ae5fa4959a186ed81f031274d8142c
Corey Alexander <corey@wellsheet.com>
2021-08-08 16:38:20 -0400

    Add eslint, prettier and TypeScript `strict` mode (#36)
    * Install and configure eslint and prettier
    
    * Run auto fix
    
    * Allow console.logs and ignore the dist dir, now actualy things to fix up next
    
    * Fix the rest of eslint, and find a bad bug I created last time
    
    * Fix some typing issues
    
    * Turn on strict mode and kinda hack it to working
    
    * I think sentry is working now :tada:
    
    * Throw more errors so we can lean into the default error handling
    
    * Keep throwing these errors after we log. No more swallowing

--- github.ts
+++ github.ts
@@ -1,136 +1,115 @@
-import { App as GithubApp } from "octokit";
-import { Channel } from "@slack/web-api/dist/response/ConversationsListResponse";
-import { SayFn, SlashCommand } from "@slack/bolt";
-import { PullRequest, PullRequestReviewComment } from "@octokit/webhooks-types";
+import { App as GithubApp } from 'octokit'
+import { Channel } from '@slack/web-api/dist/response/ConversationsListResponse'
+import { SayFn, SlashCommand } from '@slack/bolt'
+import { PullRequest, PullRequestReviewComment } from '@octokit/webhooks-types'
 
-export const addInitialComment = async (
-  githubApp: GithubApp,
-  pull: PullRequest,
-  channel: Channel
-) => {
-  const octokit = await githubApp.getInstallationOctokit(
-    parseInt(process.env.GITHUB_INSTALLATION_ID)
-  );
+export const addInitialComment = async (githubApp: GithubApp, pull: PullRequest, channel: Channel): Promise<void> => {
+  const octokit = await githubApp.getInstallationOctokit(parseInt(process.env.GITHUB_INSTALLATION_ID!))
 
   const commentBody = `A Slack Channel was created for discussion of this PR :tada:
 
 The channel name is \`${channel.name}\`. All the reviewers have been invited to the channel, and it will be archived when the PR closes.
 
-[Click Here to open the channel](https://slack.com/app_redirect?channel=${channel.id})`;
+[Click Here to open the channel](https://slack.com/app_redirect?channel=${channel.id})`
 
   try {
     await octokit.rest.issues.createComment({
-      owner: process.env.GITHUB_OWNER,
+      owner: process.env.GITHUB_OWNER!,
       repo: pull.base.repo.name,
       issue_number: pull.number,
       body: commentBody,
-    });
+    })
 
-    console.log(
-      `✅ Channel ${channel.name}: Successfully added initial comment`
-    );
+    console.log(`✅ Channel ${channel.name}: Successfully added initial comment`)
   } catch (error) {
-    console.log(`❌ Channel ${channel.name}: Failed to add initial comment`);
-    console.log(error);
+    console.log(`❌ Channel ${channel.name}: Failed to add initial comment`)
+    console.log(error)
+    throw error
   }
-};
+}
 
-export const addComment = async (
-  githubApp: GithubApp,
-  command: SlashCommand,
-  say: SayFn
-) => {
-  const octokit = await githubApp.getInstallationOctokit(
-    parseInt(process.env.GITHUB_INSTALLATION_ID)
-  );
+export const addComment = async (githubApp: GithubApp, command: SlashCommand, say: SayFn): Promise<void> => {
+  const octokit = await githubApp.getInstallationOctokit(parseInt(process.env.GITHUB_INSTALLATION_ID!))
 
-  const splitName = command.channel_name.split("-");
-  const repoName = splitName.slice(2).join("-");
+  const splitName = command.channel_name.split('-')
+  const repoName = splitName.slice(2).join('-')
 
-  const pullNumber = parseInt(splitName[1]);
+  const pullNumber = parseInt(splitName[1])
   const githubBody = `**${command.user_name}** says:\n${command.text}`
-  const slackBody = `*${command.user_name}* says:\n${command.text}\n_Comment posted to Github_`;
+  const slackBody = `*${command.user_name}* says:\n${command.text}\n_Comment posted to Github_`
 
   try {
     await octokit.rest.issues.createComment({
-      owner: process.env.GITHUB_OWNER,
+      owner: process.env.GITHUB_OWNER!,
       repo: repoName,
       issue_number: pullNumber,
       body: githubBody,
-    });
+    })
 
-    say(slackBody);
-    console.log(
-      `✅ Channel ${command.channel_name}: Successfully added a comment`
-    );
+    say(slackBody)
+    console.log(`✅ Channel ${command.channel_name}: Successfully added a comment`)
   } catch (error) {
-    console.log(`❌ Channel ${command.channel_name}: Failed to add a comment`);
-    console.log(error);
+    console.log(`❌ Channel ${command.channel_name}: Failed to add a comment`)
+    console.log(error)
+    throw error
   }
-};
+}
 
-export const getApproveReview = async (
-  githubApp: GithubApp,
-  pull: PullRequest
-) => {
+export const getApproveReview = async (githubApp: GithubApp, pull: PullRequest) => {
   try {
-    const octokit = await githubApp.getInstallationOctokit(
-      parseInt(process.env.GITHUB_INSTALLATION_ID)
-    );
+    const octokit = await githubApp.getInstallationOctokit(parseInt(process.env.GITHUB_INSTALLATION_ID!))
 
     const reviewComments = await octokit.rest.pulls.get({
-      owner: process.env.GITHUB_OWNER,
+      owner: process.env.GITHUB_OWNER!,
       repo: pull.base.repo.name,
       pull_number: pull.number,
-    });
+    })
 
-    console.log(reviewComments.data);
-    console.log(`✅ PR#${pull.number}: Successfully fetched reviews`);
-    return reviewComments;
+    console.log(reviewComments.data)
+    console.log(`✅ PR#${pull.number}: Successfully fetched reviews`)
+    return reviewComments
   } catch (error) {
-    console.log(`❌ PR#${pull.number}: Failed to fetch reviews`);
-    console.log(error);
+    console.log(`❌ PR#${pull.number}: Failed to fetch reviews`)
+    console.log(error)
+    throw error
   }
-};
+}
 
 export const getReviewComments = async (
   githubApp: GithubApp,
-  pull: Pick<PullRequest, "number" | "base">,
-) => {
+  pull: Pick<PullRequest, 'number' | 'base'>,
+): Promise<PullRequestReviewComment[]> => {
   try {
-    const octokit = await githubApp.getInstallationOctokit(
-      parseInt(process.env.GITHUB_INSTALLATION_ID)
-    );
+    const octokit = await githubApp.getInstallationOctokit(parseInt(process.env.GITHUB_INSTALLATION_ID!))
 
     const reviewComments = await octokit.rest.pulls.listReviewComments({
-      owner: process.env.GITHUB_OWNER,
+      owner: process.env.GITHUB_OWNER!,
       repo: pull.base.repo.name,
       pull_number: pull.number,
-    });
+    })
 
-    console.log(`✅ PR#${pull.number}: Successfully fetched review comments`);
-    return reviewComments.data as PullRequestReviewComment[];
+    console.log(`✅ PR#${pull.number}: Successfully fetched review comments`)
+    return reviewComments.data as PullRequestReviewComment[]
   } catch (error) {
-    console.log(`❌ PR#${pull.number}: Failed to fetch review comments`);
-    console.log(error);
+    console.log(`❌ PR#${pull.number}: Failed to fetch review comments`)
+    console.log(error)
+    throw error
   }
-};
+}
 
 export const postReviewComentReply = async (
   githubApp: GithubApp,
-  pull: Pick<PullRequest,'number' | 'base'>,
+  pull: Pick<PullRequest, 'number' | 'base'>,
   comment_id: number,
   reply: string,
 ) => {
-    const octokit = await githubApp.getInstallationOctokit(
-      parseInt(process.env.GITHUB_INSTALLATION_ID)
-    );
+  const octokit = await githubApp.getInstallationOctokit(parseInt(process.env.GITHUB_INSTALLATION_ID!))
 
-    return octokit.rest.pulls.createReplyForReviewComment({
-      owner: process.env.GITHUB_OWNER,
-      repo: pull.base.repo.name,
-      pull_number: pull.number,
-      comment_id,
-      body: reply,
-    })
+  return octokit.rest.pulls.createReplyForReviewComment({
+    owner: process.env.GITHUB_OWNER!,
+    repo: pull.base.repo.name,
+    pull_number: pull.number,
+    comment_id,
+    body: reply,
+  })
 }
--- slack.ts
+++ slack.ts
@@ -1,58 +1,56 @@
-import { PullRequest, User } from "@octokit/webhooks-types";
-import { App as SlackApp } from "@slack/bolt";
-import { Channel } from "@slack/web-api/dist/response/ConversationsListResponse";
-import { Message } from "@slack/web-api/dist/response/ConversationsHistoryResponse";
+import { PullRequest, User } from '@octokit/webhooks-types'
+import { App as SlackApp } from '@slack/bolt'
+import { Channel } from '@slack/web-api/dist/response/ConversationsListResponse'
+import { Message } from '@slack/web-api/dist/response/ConversationsHistoryResponse'
 
-export const gitUserToSlackId = JSON.parse(process.env.GIT_USER_TO_SLACK_ID);
+export const gitUserToSlackId = JSON.parse(process.env.GIT_USER_TO_SLACK_ID!)
 
 const paginate = async <T>(
   slackApp: SlackApp,
-  initialQuery: any,
-  extractElements: (query: any) => T[]
+  initialQuery: any, // eslint-disable-line @typescript-eslint/no-explicit-any
+  extractElements: (query: any) => T[], // eslint-disable-line @typescript-eslint/no-explicit-any
 ): Promise<T[]> => {
-  let collection = extractElements(initialQuery);
+  let collection = extractElements(initialQuery)
 
-  let nextCursor = initialQuery.response_metadata.next_cursor;
+  let nextCursor = initialQuery.response_metadata.next_cursor
   while (nextCursor) {
+    // TODO: Ummmm this is very broken lol
+    // The method below is hard-coded. Need to fix this
     const moreElements = await slackApp.client.conversations.list({
       cursor: nextCursor,
-    });
+    })
 
-    collection = [...collection, ...extractElements(moreElements)];
-    nextCursor = moreElements.response_metadata.next_cursor;
+    collection = [...collection, ...extractElements(moreElements)]
+    nextCursor = moreElements?.response_metadata?.next_cursor
   }
 
-  return collection;
-};
+  return collection
+}
 
-export const getSlackChannels = async (slackApp: SlackApp) => {
+export const getSlackChannels = async (slackApp: SlackApp): Promise<Channel[]> => {
   try {
-    const initChannels = await slackApp.client.conversations.list();
-    const allChannels: Channel[] = await paginate(
-      slackApp,
-      initChannels,
-      (x) => x.channels
-    );
+    const initChannels = await slackApp.client.conversations.list()
+    const allChannels: Channel[] = await paginate(slackApp, initChannels, x => x.channels)
 
-    console.log("✅ Success - fetched all channels");
-    return allChannels;
+    console.log('✅ Success - fetched all channels')
+    return allChannels
   } catch (error) {
-    console.log("❌ Error - fetched all channels");
-    console.log(error);
+    console.log('❌ Error - fetched all channels')
+    console.log(error)
+    throw error
   }
-};
+}
 
-export const getChannelHistory = async (
-  slackApp: SlackApp,
-  channel: Channel
-): Promise<Message[]> => {
+export const getChannelHistory = async (slackApp: SlackApp, channel: Channel): Promise<Message[]> => {
+  if (!channel.id) return []
+
   const botCommentResponse = await slackApp.client.conversations.history({
     channel: channel.id,
-    oldest: "0",
-  });
+    oldest: '0',
+  })
 
-  return await paginate(slackApp, botCommentResponse, (x) => x.messages);
-};
+  return await paginate(slackApp, botCommentResponse, x => x.messages)
+}
 
 export const slackTextFromPullRequest = (pull: PullRequest): string => {
   return `
@@ -63,93 +61,82 @@ PR Description:
 \`\`\`
 ${pull.body}
 \`\`\`
-`;
-};
+`
+}
 
-export const createPullChannel = async (
-  slackApp: SlackApp,
-  pull: PullRequest
-): Promise<Channel> => {
+export const createPullChannel = async (slackApp: SlackApp, pull: PullRequest): Promise<Channel> => {
   try {
     const newChannel = await slackApp.client.conversations.create({
       name: `pr-${pull.number}-${pull.base.repo.name}`,
-    });
+    })
+    const newChannelId = newChannel.channel!.id!
 
-    const text = slackTextFromPullRequest(pull);
+    const text = slackTextFromPullRequest(pull)
     await slackApp.client.chat.postMessage({
-      channel: newChannel.channel.id,
+      channel: newChannelId,
       text,
       unfurl_links: false,
-    });
+    })
 
     // add a topic to the channel
     await slackApp.client.conversations.setTopic({
-      channel: newChannel.channel.id,
+      channel: newChannelId,
       topic: pull.title,
-    });
+    })
 
-    console.log(`✅ PR#${pull.number}: Successfully created channel`);
-    return newChannel.channel;
+    if (!newChannel.channel) throw 'Channel was not in createChannelResponse'
+
+    console.log(`✅ PR#${pull.number}: Successfully created channel`)
+    return newChannel.channel
   } catch (error) {
-    console.log(`❌ PR#${pull.number}: Failed to create channel`);
-    console.log(error);
+    console.log(`❌ PR#${pull.number}: Failed to create channel`)
+    console.log(error)
+    throw error
   }
-};
+}
 
-const getAllMembers = async (
-  slackApp: SlackApp,
-  pull: PullRequest,
-  channel: Channel
-) => {
+const getAllMembers = async (slackApp: SlackApp, pull: PullRequest, channel: Channel) => {
+  if (!channel.id) return []
+
   try {
     const members = await slackApp.client.conversations.members({
       channel: channel.id,
-    });
-    const allMembers: string[] = await paginate(
-      slackApp,
-      members,
-      (x) => x.members
-    );
+    })
+    const allMembers: string[] = await paginate(slackApp, members, x => x.members)
 
-    console.log(`✅ PR#${pull.number}: Successfully fetched all slack members`);
-    return allMembers;
+    console.log(`✅ PR#${pull.number}: Successfully fetched all slack members`)
+    return allMembers
   } catch (error) {
-    console.log(`❌ PR#${pull.number}: Failed to fetched all slack members`);
-    console.log(error);
+    console.log(`❌ PR#${pull.number}: Failed to fetched all slack members`)
+    console.log(error)
+    throw error
   }
-};
+}
 
-export const addReviewersToChannel = async (
-  slackApp: SlackApp,
-  pull: PullRequest,
-  channel: Channel
-) => {
+export const addReviewersToChannel = async (slackApp: SlackApp, pull: PullRequest, channel: Channel) => {
   try {
-    const allMembers = await getAllMembers(slackApp, pull, channel);
+    const allMembers = await getAllMembers(slackApp, pull, channel)
 
     const reviewers = pull.requested_reviewers
-      .map((reviewer: User) => gitUserToSlackId[reviewer.login])
-      .concat(gitUserToSlackId[pull.user.login]);
+      .map(reviewer => 'login' in reviewer && gitUserToSlackId[reviewer.login])
+      .concat(gitUserToSlackId[pull.user.login])
 
-    const reviewerToInvite = reviewers.some(
-      (reviewer) => !allMembers.includes(reviewer)
-    );
+    const reviewerToInvite = reviewers.some(reviewer => !allMembers || !allMembers.includes(reviewer))
 
-    const reviewersString = reviewers.join(",");
+    const reviewersString = reviewers.join(',')
 
-    if (reviewerToInvite) {
+    if (reviewerToInvite && channel.id) {
       await slackApp.client.conversations.invite({
         channel: channel.id,
         emails: [],
         users: reviewersString,
-      });
+      })
     }
 
-    console.log(`✅ PR#${pull.number}: Successfully added ${reviewersString}`);
+    console.log(`✅ PR#${pull.number}: Successfully added ${reviewersString}`)
   } catch (error) {
-    console.log(
-      `❌ PR#${pull.number}: Failed to add requested reviewers to slack channel`
-    );
-    console.log(error);
+    console.log(`❌ PR#${pull.number}: Failed to add requested reviewers to slack channel`)
+    console.log(error)
+    throw error
   }
-};
+}
